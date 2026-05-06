65f19f3a62f36a9fd6a70969e95d78f2566e13ca
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-05-06 10:46:51 -0700

    Update puppy_state.py

--- puppy_state.py
+++ puppy_state.py
@@ -1,3 +1,17 @@
 '''
+Name: Jacob Miranda & Daniel Puerto
+Date: 5/6/26
+Group: 11
+Description:
 '''
 
+from abc import ABC, abstractmethod
+
+class PuppyState(ABC):
+    @abstractmethod
+    def feed(self, puppy):
+        pass
+
+    @abstractmethod
+    def play(self, puppy):
+        pass
