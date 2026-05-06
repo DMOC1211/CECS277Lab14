45e3a2b3d3a8780a3027f07fe88ecdbae960c52f
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-05-06 10:47:36 -0700

    Update state_play.py

--- state_play.py
+++ state_play.py
@@ -1,2 +1,26 @@
 '''
+Name: Jacob Miranda & Daniel Puerto
+Date: 5/6/26
+Group: 11
+Description:
 '''
+
+from puppy_state import PuppyState
+from state_asleep import StateAsleep
+from state_eat import StateEat
+
+
+class StatePlay(PuppyState):
+    def feed(self, puppy):
+        puppy.change_state(StateEat())
+        puppy.inc_feeds()
+        return "The puppy stops playing and runs over for food."
+
+    def play(self, puppy):
+        puppy.inc_plays()
+
+        if puppy.plays >= 3:
+            puppy.change_state(StateAsleep())
+            return "The puppy gets tired from playing and falls asleep."
+
+        return "The puppy keeps playing happily!"
