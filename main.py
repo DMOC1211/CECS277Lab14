23a5ec82f5f5720fc61ab65a24a47cc030eb6eac
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-05-06 10:40:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 '''
+Name: Jacob Miranda & Daniel Puerto
+Date: 5/6/26
+Group: 11
+Description:
 '''
+
+
+from puppy import Puppy
+
+def main():
+    p = Puppy()
+
+    print(p.give_food())
+    print(p.throw_ball())
+    print(p.throw_ball())
+    print(p.give_food())
+
+if __name__ == "__main__":
+    main()
